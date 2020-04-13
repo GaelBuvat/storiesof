7611d090e832029c0009fb2662e1d3a4a1cc0ff3
gaelbuvat <buvatgael@gmail.com>
2020-04-13 17:56:11 +0200

    pip freeze h10

--- storiesof/settings.py
+++ storiesof/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['crop-agri.herokuapp.com']
+ALLOWED_HOSTS = ['storiesof.herokuapp.com/']
 
 SERVER_EMAIL = 'buvatgael@gmail.com'
 
